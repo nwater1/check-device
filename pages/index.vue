69a373f75bcb5b8e3ec8f55dbf73ec12167f4a6d
Mint <mint167349@gmail.com>
2021-05-25 13:50:15 +0700

    check

--- pages/index.vue
+++ pages/index.vue
@@ -46,11 +46,11 @@ export default {
   methods: {
     chooseDevice () {
       if ((this.url === 'smartphone' && this.deviceType === 'iPhone') || (this.url === 'smartphone' && this.deviceType === 'iPad')) {
-        window.location = 'www.google.com';
+        window.location.href = 'www.google.com';
       } if (this.url === 'pc') {
         return 'https://page.line.me/?accountId=002sjkcf&openerPlatform=native&openerKey=talkroom:header'
       } if (this.url === 'smartphone' && this.deviceType === 'Android') {
-        window.location = 'www.google.com';
+        window.location.href = 'www.google.com';
     }
   }
 }
